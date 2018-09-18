3efbddfd2956e798005e4eba34d7cbfbde93e95a
kiyami <kiyami_erdim@hotmail.com>
2018-09-18 09:42:15 +0300

    editted

--- maximum_likelyhood.py
+++ maximum_likelyhood.py
@@ -7,10 +7,9 @@ from pylab import *
 import random
 
 #########################################################################
-# Asymmetric error calculation with maximum likelihood estimator (MLE)
+# Plotting Maximum Likelihood Estimation (MLE) of an asymmetric (non-gaussian) distribution
 # A and B values with asymmetric errors
 # (A_neg -> minus error, A_poz -> plus error)
-# Operation -> select the operation between A and B (A+B, A-B...)
 # N -> number of produced random value
 # delta_L -> confidence interval
 # (0.5 ML interval is means 1 sigma confidence interval)
@@ -27,9 +26,6 @@ graphp_limits_sum = [10,20]
 
 #confidence interval
 delta_L = 0.5
-
-# select an operation (+,-,*,/)
-operation = "+"
 
 #########################################################################
 
@@ -95,14 +91,16 @@ def find_sigma(chi_list,x_plot,N,delta_L):
 
 def plot_MLE(MLE,x_plot,max_value,max_x,right_sigma,left_sigma):
 	plt.plot(x_plot,MLE)
-	plt.axhline(y=max_value, color='red', linestyle='-', linewidth=1.4, label='BCG')
-	plt.axhline(y=max_value-delta_L, color='red', linestyle='-', linewidth=1.4, label='BCG')
+	plt.axhline(y=max_value, color='red', linestyle='-', linewidth=1.4, label='Mean')
+	plt.axhline(y=max_value-delta_L, color='green', linestyle='-', linewidth=1.8, label='1-Sigma')
+	plt.axhline(y=max_value-2.0*delta_L, color='green', linestyle='-', linewidth=1.2, label='2-Sigma')
+	plt.axhline(y=max_value-3.0*delta_L, color='green', linestyle='-', linewidth=0.6, label='3-Sigma')
 
 	plt.axvline(x=max_x, color='black', linestyle='-', linewidth=1.4, label='BCG')
-	plt.axvline(x=right_sigma, color='black', linestyle='--', linewidth=1.4, label='BCG')
-	plt.axvline(x=left_sigma, color='black', linestyle='--', linewidth=1.4, label='BCG')
+	plt.axvline(x=right_sigma, color='black', linestyle='--', linewidth=1.4)
+	plt.axvline(x=left_sigma, color='black', linestyle='--', linewidth=1.4)
 
-
+	plt.legend()
 	plt.show()
 
 
@@ -113,38 +111,15 @@ def plot_MLE(MLE,x_plot,max_value,max_x,right_sigma,left_sigma):
 MLE_A = mle(A,A_neg,A_poz,N)
 MLE_B = mle(B,B_neg,B_poz,N)
 
-
-MLE_sum = []
-
-if str(operation) == "+":
-	for i in range(N):
-		MLE_sum = np.append(MLE_sum, MLE_A[i]+MLE_B[i])
-elif str(operation) == "-":
-	for i in range(N):
-		MLE_sum = np.append(MLE_sum, (MLE_A[i]-MLE_B[i]))
-elif str(operation) == "/":
-	for i in range(N):
-		MLE_sum = np.append(MLE_sum, -(MLE_A[i]/MLE_B[i]))
-elif str(operation) == "*":
-	for i in range(N):
-		MLE_sum = np.append(MLE_sum, -(MLE_A[i]*MLE_B[i]))
-else:
-	print("\n#######################################")
-	print("#        Invalid Operation!           #")
-	print("#######################################\n")
-
-
 x_plot_A = np.linspace(graphp_limits_A[0],graphp_limits_A[1],N)
 x_plot_B = np.linspace(graphp_limits_B[0],graphp_limits_B[1],N)
-x_plot_sum = np.linspace(graphp_limits_sum[0],graphp_limits_sum[1],N)
 
 left_sigma_A, right_sigma_A, max_A, max_value_A = find_sigma(MLE_A,x_plot_A,N,delta_L)
-left_sigma_B, right_sigma_B, max_B, max_value_B = find_sigma(MLE_B,x_plot_B,N,delta_L)
-left_sigma_sum, right_sigma_sum, max_sum, max_value_sum = find_sigma(MLE_sum,x_plot_sum,N,delta_L)
+#left_sigma_B, right_sigma_B, max_B, max_value_B = find_sigma(MLE_B,x_plot_B,N,delta_L)
 
 #########################################################################
 
-plot_MLE(MLE_sum,x_plot_sum,max_value_sum,max_sum,right_sigma_sum,left_sigma_sum)
+plot_MLE(MLE_A,x_plot_A,max_value_A,max_A,right_sigma_A,left_sigma_A)
 
 
 
